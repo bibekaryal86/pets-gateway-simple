d0337fc39a4086d752e9c1081314a8e2c1abc880
Bibek Aryal <bibekaryal86@gmail.com>
2022-01-05 11:11:04 -0700

    Update GatewayFilter and GatewayServlet

--- app/src/main/java/pets/gateway/app/filter/GatewayFilter.java
+++ app/src/main/java/pets/gateway/app/filter/GatewayFilter.java
@@ -6,23 +6,29 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 public class GatewayFilter implements Filter {
 
+    private static final String TRACE = "TRACE";
+
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletRequest.setAttribute("TRACE", ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE));
+
         logRequest(httpServletRequest);
         chain.doFilter(request, response);
         logResponse(httpServletRequest, httpServletResponse);
     }
 
     private void logRequest(HttpServletRequest httpServletRequest) {
-        log.info("REQUEST BEGIN: [ {} ] | [ {} ]", httpServletRequest.getRequestURI(), httpServletRequest.getRemoteAddr());
+        log.info("REQUEST::: [ {} ] | Incoming: [ {} ] | Method: [ {} ]",
+                httpServletRequest.getAttribute(TRACE), httpServletRequest.getRequestURI(), httpServletRequest.getMethod());
     }
 
     private void logResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        log.info("REQUEST END: [ {} ] | [ {} ]", httpServletRequest.getRequestURI(), httpServletResponse.getStatus());
+        log.info("RESPONSE::: [ {} ] | Status [ {} ]", httpServletRequest.getAttribute(TRACE), httpServletResponse.getStatus());
     }
 }
--- app/src/main/java/pets/gateway/app/servlet/GatewayServlet.java
+++ app/src/main/java/pets/gateway/app/servlet/GatewayServlet.java
@@ -5,15 +5,84 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class GatewayServlet extends HttpServlet {
+    // find a way to set response from actual response from downstream service
+
+    private boolean isTestPing(String requestUri) {
+        return requestUri.contains("/tests/ping");
+    }
+
+    private String trace(HttpServletRequest request) {
+        return Objects.requireNonNullElse(request.getAttribute("TRACE"), "TRACE_ERROR").toString();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // do all reset actions here
         response.setCharacterEncoding("utf-8");
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.setContentType("application/json");
-        response.setStatus(200);
-        response.getWriter().print("{\"reset\": \"successful\"}");
+
+        System.out.println("in doGet: " + trace(request));
+
+        if (isTestPing(request.getRequestURI())) {
+            response.setStatus(200);
+            response.getWriter().print("{\"ping\": \"successful\"}");
+        } else {
+            response.setStatus(200);
+            response.getWriter().print("{\"ping\": \"unsuccessful\"}");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.setContentType("application/json");
+
+        System.out.println("in doPost: " + request.getAttribute("TRACE"));
+
+        if (isTestPing(request.getRequestURI())) {
+            response.setStatus(405);
+            response.getWriter().print("{\"ping\": \"method not allowed\"}");
+        } else {
+            response.setStatus(200);
+            response.getWriter().print("{\"ping\": \"something else\"}");
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.setContentType("application/json");
+
+        System.out.println("in doPut: " + trace(request));
+
+        if (isTestPing(request.getRequestURI())) {
+            response.setStatus(405);
+            response.getWriter().print("{\"ping\": \"method not allowed\"}");
+        } else {
+            response.setStatus(200);
+            response.getWriter().print("{\"ping\": \"something else\"}");
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.setContentType("application/json");
+
+        System.out.println("in doDelete: " + trace(request));
+
+        if (isTestPing(request.getRequestURI())) {
+            response.setStatus(405);
+            response.getWriter().print("{\"ping\": \"method not allowed\"}");
+        } else {
+            response.setStatus(200);
+            response.getWriter().print("{\"ping\": \"something else\"}");
+        }
     }
 }
