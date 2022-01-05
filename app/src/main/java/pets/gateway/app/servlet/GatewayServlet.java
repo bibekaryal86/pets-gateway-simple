602cce26ef6f7e7e3880ec33e9b24b42c2a75995
Bibek Aryal <bibekaryal86@gmail.com>
2022-01-05 11:22:45 -0700

    Update GatewayFilter and GatewayServlet

--- app/src/main/java/pets/gateway/app/servlet/GatewayServlet.java
+++ app/src/main/java/pets/gateway/app/servlet/GatewayServlet.java
@@ -9,6 +9,12 @@ import java.util.Objects;
 
 public class GatewayServlet extends HttpServlet {
     // find a way to set response from actual response from downstream service
+    private static final String CHARACTER_ENCODING = "utf-8";
+    private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    private static final String CONTENT_TYPE = "application/json";
+    private static final String PING_SUCCESSFUL = "{\"ping\": \"successful\"}";
+    private static final String PING_UNSUCCESSFUL = "{\"ping\": \"method not allowed\"}";
+
 
     private boolean isTestPing(String requestUri) {
         return requestUri.contains("/tests/ping");
@@ -20,69 +26,69 @@ public class GatewayServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setCharacterEncoding("utf-8");
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.setContentType("application/json");
+        response.setCharacterEncoding(CHARACTER_ENCODING);
+        response.addHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        response.setContentType(CONTENT_TYPE);
 
         System.out.println("in doGet: " + trace(request));
 
         if (isTestPing(request.getRequestURI())) {
             response.setStatus(200);
-            response.getWriter().print("{\"ping\": \"successful\"}");
+            response.getWriter().print(PING_SUCCESSFUL);
         } else {
             response.setStatus(200);
-            response.getWriter().print("{\"ping\": \"unsuccessful\"}");
+            response.getWriter().print(PING_UNSUCCESSFUL);
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setCharacterEncoding("utf-8");
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.setContentType("application/json");
+        response.setCharacterEncoding(CHARACTER_ENCODING);
+        response.addHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        response.setContentType(CONTENT_TYPE);
 
         System.out.println("in doPost: " + request.getAttribute("TRACE"));
 
         if (isTestPing(request.getRequestURI())) {
             response.setStatus(405);
-            response.getWriter().print("{\"ping\": \"method not allowed\"}");
+            response.getWriter().print(PING_UNSUCCESSFUL);
         } else {
             response.setStatus(200);
-            response.getWriter().print("{\"ping\": \"something else\"}");
+            response.getWriter().print(PING_UNSUCCESSFUL);
         }
     }
 
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setCharacterEncoding("utf-8");
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.setContentType("application/json");
+        response.setCharacterEncoding(CHARACTER_ENCODING);
+        response.addHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        response.setContentType(CONTENT_TYPE);
 
         System.out.println("in doPut: " + trace(request));
 
         if (isTestPing(request.getRequestURI())) {
             response.setStatus(405);
-            response.getWriter().print("{\"ping\": \"method not allowed\"}");
+            response.getWriter().print(PING_UNSUCCESSFUL);
         } else {
             response.setStatus(200);
-            response.getWriter().print("{\"ping\": \"something else\"}");
+            response.getWriter().print(PING_UNSUCCESSFUL);
         }
     }
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setCharacterEncoding("utf-8");
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.setContentType("application/json");
+        response.setCharacterEncoding(CHARACTER_ENCODING);
+        response.addHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        response.setContentType(CONTENT_TYPE);
 
         System.out.println("in doDelete: " + trace(request));
 
         if (isTestPing(request.getRequestURI())) {
             response.setStatus(405);
-            response.getWriter().print("{\"ping\": \"method not allowed\"}");
+            response.getWriter().print(PING_UNSUCCESSFUL);
         } else {
             response.setStatus(200);
-            response.getWriter().print("{\"ping\": \"something else\"}");
+            response.getWriter().print(PING_UNSUCCESSFUL);
         }
     }
 }
